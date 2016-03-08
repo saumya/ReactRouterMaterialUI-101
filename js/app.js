25531e1d871e5f1059d9f6949e01a5a3035d97dc
saumya <2saumyaray@gmail.com>
2016-03-08 13:52:11 +0530

    added the react-tap-event plugin

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
   console.log('Reaction : 0.1.0');
   var React = require('react');
   var ReactDOM = require('react-dom');
+  // ref : https://www.npmjs.com/package/react-tap-event-plugin
+  var injectTapEventPlugin = require("react-tap-event-plugin");
+  injectTapEventPlugin();
   //
   var Router = require('react-router').Router;
   var Route = require('react-router').Route;
@@ -15,8 +18,6 @@
   var SeeAllControllerView = require('./components/controllerViewSeeAll.react');
   var AddNewControllerView = require('./components/controllerViewAddNew.react');
   //
-  // Route declaration : type : 1
-  // IndexRoute : the dafault route or the default view to render
   var routes = (
                 <Route path="/" component={AppControllerView}>
                   <IndexRoute component={HomeControllerView} />
@@ -24,17 +25,6 @@
                   <Route path="add" component={AddNewControllerView} />
                 </Route>
               );
-
-  /*
-  // Route declaration : type : 2
-  var routes = {
-    path: '/',
-    component: App,
-    childRoutes: [
-      { path: 'one', component: One },
-      { path: 'two', component: Two },
-    ]
-  }
-  */
+  //
   var App = ReactDOM.render(<Router routes={routes} history={hashHistory} />,document.getElementById('react-app'));
 })();
